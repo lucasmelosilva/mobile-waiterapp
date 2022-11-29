e2e0ea0080a8fccd9973169cff90f003b7de72c4
lucas melo <lucas.melo044@gmail.com>
2022-11-29 01:15:42 -0300

    add main componente

--- App.tsx
+++ App.tsx
@@ -1,22 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-
-import { Text } from "./src/components/Text";
+import { useFonts } from "expo-font";
+import { Main } from "./src/Main";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-700": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Bem Vindo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
