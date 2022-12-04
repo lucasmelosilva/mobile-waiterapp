1e2d273b714e4b18c493c6af8d9131d8eb6a06fb
lucas melo <lucas.melo044@gmail.com>
2022-12-04 11:42:21 -0300

    alteracao do header quando estiver com uma mesa selecionada

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -20,10 +20,17 @@ export function Main() {
     setSelectedTable(table);
   }
 
+  function handleCancelOrder() {
+    setSelectedTable("");
+  }
+
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          onCancelOrder={handleCancelOrder}
+          selectedTable={selectedTable}
+        />
 
         <CategoriesContainer>
           <Categories />
