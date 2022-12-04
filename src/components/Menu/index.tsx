3e07aba7cc1c883d11aac0728ca394f389160693
lucas melo <lucas.melo044@gmail.com>
2022-12-04 18:17:34 -0300

    add types product

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import { FlatList } from "react-native";
 
 import { products } from "../../mocks/products";
 import { formatCurreny } from "../../utils/formatCurrency";
 import { PlusCircle } from "../Icons/PlusCircle";
+import { ProductModal } from "../ProductModal";
 import { Text } from "../Text";
 
+import { Product } from "../../types/Product";
+
 import {
-  Product,
+  ProductContainer,
   ProductImage,
   ProductDatails,
   Separator,
@@ -14,36 +18,57 @@ import {
 } from "./styled";
 
 export function Menu() {
-  return (
-    <FlatList
-      data={products}
-      style={{ marginTop: 32 }}
-      contentContainerStyle={{ paddingHorizontal: 24 }}
-      keyExtractor={(product) => product._id}
-      showsVerticalScrollIndicator={false}
-      ItemSeparatorComponent={Separator}
-      renderItem={({ item: product }) => (
-        <Product>
-          <ProductImage
-            source={{
-              uri: `http://192.168.0.7:3001/uploads/${product.imagePath}`,
-            }}
-          />
-          <ProductDatails>
-            <Text weight="600">{product.name}</Text>
-            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
-              {product.description}
-            </Text>
-            <Text size={14} weight="600">
-              {formatCurreny(product.price)}
-            </Text>
-          </ProductDatails>
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
 
-          <AddToCartButton>
-            <PlusCircle />
-          </AddToCartButton>
-        </Product>
-      )}
-    />
+  function handleOpenModal(product: Product) {
+    setIsModalVisible(true);
+    setSelectedProduct(product);
+  }
+
+  function handleCloseModal() {
+    setIsModalVisible(false);
+    setSelectedProduct(null);
+  }
+
+  return (
+    <>
+      <ProductModal
+        visible={isModalVisible}
+        onClose={handleCloseModal}
+        product={selectedProduct}
+      ></ProductModal>
+
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={(product) => product._id}
+        showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <ProductContainer onPress={() => handleOpenModal(product)}>
+            <ProductImage
+              source={{
+                uri: `http://192.168.0.7:3001/uploads/${product.imagePath}`,
+              }}
+            />
+            <ProductDatails>
+              <Text weight="600">{product.name}</Text>
+              <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight="600">
+                {formatCurreny(product.price)}
+              </Text>
+            </ProductDatails>
+
+            <AddToCartButton>
+              <PlusCircle />
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   );
 }
